5f9b93630cd4560eddf6bd8d612ae78b89cd0a3e
IamSanjid <iamsanjidhaque@gmail.com>
2018-10-23 13:35:26 +0600

    New script

---
+++ Route7 - Celedon.lua
@@ -0,0 +1,43 @@
+name = "Leveling: Route 7 (near Celadon)"
+author = "Tester"
+description = [[This script will train the first pokémon of your team.
+It will also try to capture shinies by throwing pokéballs.
+Start anywhere between Celadon City and Route 7.]]
+
+function onPathAction()
+	if isPokemonUsable(1) then
+		if stringContains(getAreaName(), "Center") and stringContains(getAreaName(), "Celadon") then
+			moveToCell(12, 84)
+		elseif getAreaName() == "Celadon City" then
+			moveToArea("Route 7")
+		elseif getAreaName() == "Route 7" then
+			moveLinearX(203, 209, 88)
+		end
+	else
+		if getAreaName() == "Route 7" then
+			moveToArea("Celadon City")
+		elseif getAreaName() == "Celadon City" then
+			moveToCell(165, 82)
+		elseif stringContains(getAreaName(), "Center") and stringContains(getAreaName(), "Celadon") then
+			usePokecenter()
+		end
+	end
+end
+
+function onBattleAction()
+	if isWildBattle() and isOpponentShiny() then
+		if useItem("Ultra Ball") or useItem("Great Ball") or useItem("Poke Ball") then
+			return
+		end
+	end
+	if getActivePokemonNumber() == 1 then
+		return attack() or sendUsablePokemon() or run() or sendAnyPokemon()
+	else
+		return run() or attack() or sendUsablePokemon() or sendAnyPokemon()
+	end
+end
+
+function onLearningMove(moveName, pokemonIndex)
+	forgetAnyMoveExcept({"Ember", "Dragon Rage", "Vine Whip", "Tackle", "Fire Fang", "Water Gun"})
+	log("[INFO] A Pokemon is trying to learn "..moveName .. "!")
+end
